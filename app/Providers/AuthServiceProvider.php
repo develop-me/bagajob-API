7cc474d5c6df65aadb8a5ba2bad2f29bd790afb4
Mhairi Hislop <mhairihislop@Mhairis-MacBook-Pro.local>
2020-08-25 20:27:31 +0100

    authorisation with passport set up minus routes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,8 +24,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
 
         //
+        Passport::routes();
     }
 }
